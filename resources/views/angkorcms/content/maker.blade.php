dec18c3b75ccbfda58820a809639097434fe60b7
Lucas Burdeyron <lucas.burdeyron@gmail.com>
2015-06-23 17:36:13 +0700

    Stop rewriting URL

--- resources/views/angkorcms/content/maker.blade.php
+++ resources/views/angkorcms/content/maker.blade.php
@@ -30,6 +30,7 @@ tinymce.init({
     toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
     toolbar2: "print preview media | forecolor backcolor emoticons",
     image_advtab: true,
+    relative_urls : false,
     templates: [
         {title: 'Test template 1', content: 'Test 1'},
         {title: 'Test template 2', content: 'Test 2'}
