0173f01a636634ac5e063604d0c55a4eaac92662
Lucas Burdeyron <lubur@smile.fr>
2015-12-08 17:29:45 +0100

    Some comments in FrontController

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -21,6 +21,7 @@ class FrontController extends Controller {
 	}
 
 	public function indexWithParameters($slug, $parameters) {
+        //get parameters array form string
 		if ($parameters) {
 			$parameters = explode('/', $parameters);
 			$nbParams = count($parameters);
@@ -36,20 +37,23 @@ class FrontController extends Controller {
 	}
 
 	public function indexGeneral($slug, $parameters = array()) {
+        //Get the slug
 		$page_trans = $this->page_trans_repository->getBySlug($slug);
 
 		$nextPath = '';
 		if ($page_trans == null) {
 			if (count($parameters) > 0) {
+                // Redirect to the right index if no page found and parameters used
 				if (isset($parameters[0])) {
 					$parameters[$slug] = $parameters[0];
 					unset($parameters[0]);
 				}
 				$nextPath = '';
 				foreach ($parameters as $key => $value) {
-					$nextPath = '/' . $key . '/' . $value;
+					$nextPath .= '/' . $key . '/' . $value;
 				}
 				Session::reflash();
+
 				$newUrl = Session::get('language')->code . '/' . Config::get('angkorcmspages.alias.' . Session::get('language')->code . '.index') . $nextPath;
 				return Redirect::to($newUrl);
 			} else {
@@ -57,6 +61,7 @@ class FrontController extends Controller {
 			}
 		}
 
+        // If page not accessible
 		if (!$page_trans->page->accessible) {
 			App::abort(404);
 		}
@@ -64,6 +69,7 @@ class FrontController extends Controller {
 		// Page translation checking.
 		if ($page_trans->lang_id != Session::get('language')->id) {
 			$page = $page_trans->page;
+            // Get the right URL that match with language
 			$newSlug = $this->page_trans_repository->getSlugByPageLang($page->id, Session::get('language')->id);
 			if ($newSlug != null) {
 				$nextPath = '';
@@ -75,20 +81,24 @@ class FrontController extends Controller {
 			}
 		}
 
+
 		foreach ($parameters as $key => $value) {
-			$nextPath = '/' . $key . '/' . $value;
+			$nextPath .= '/' . $key . '/' . $value;
 		}
+
+        //GET PARAMETERS
 		$url_base = URL::to($slug);
 		$parameters['slug'] = $slug;
 		$parameters['url_base'] = $url_base;
 		$parameters['parameters'] = $nextPath;
 		$parameters['end_url'] = $slug . $nextPath;
 
+        // GET THEME/TEMPLATE
 		$theme = $page_trans->page->theme;
 		$template = $theme->template;
 
+        // GET BLOCKs
 		$listBlocks = $page_trans->blocks;
-
 		$blocks = array();
 		foreach ($template->blocks as $value) {
 			$blocks[$value->name] = array();
@@ -96,12 +106,16 @@ class FrontController extends Controller {
 		foreach ($listBlocks as $block) {
 			$blocks[$block->block->name] = $block->modules;
 		}
-		$data = array('title' => $page_trans->title, 'blocks' => $blocks, 'parameters' => $parameters);
-		//return $blocks;
 
+        // Set datas
+		$data = array('title' => $page_trans->title, 'blocks' => $blocks, 'parameters' => $parameters);
+
+        // GET VIEW
 		$view = "templates/" . $template->name . "/" . $theme->name . "/" . $theme->view;
 		$view = str_replace(".blade.php", "", $view);
-		return view($view, $data);
+
+        // return view with data
+        return view($view, $data);
 	}
 
 	/*public function test() {
